b9d6e9e1c4e9806d3ad3f7516e95b43dd5ebbfa0
Ivan Smirnov <ivan.s.2001@yandex.ru>
2026-04-16 14:46:07 +0300

    1

--- heroes/admin.py
+++ heroes/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import Hero, HeroPage, PageSection
 
@@ -7,6 +9,14 @@ class HeroPageInline(admin.TabularInline):
     model = HeroPage
     extra = 0
     fields = ('title', 'code', 'slug', 'tab_label', 'order', 'is_published')
+    ordering = ('order', 'title')
+    show_change_link = True
+
+
+class PageSectionInline(admin.TabularInline):
+    model = PageSection
+    extra = 0
+    fields = ('title', 'code', 'slug', 'order', 'is_published')
     ordering = ('order', 'title')
     show_change_link = True
 
@@ -35,6 +45,44 @@ class HeroPageAdmin(admin.ModelAdmin):
     list_filter = ('hero', 'is_published')
     ordering = ('hero__name', 'order', 'title')
     list_select_related = ('hero',)
+    inlines = (PageSectionInline,)
 
 
-admin.site.register(PageSection)
+@admin.register(PageSection)
+class PageSectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'code',
+        'page_link',
+        'hero_link',
+        'order',
+        'is_published',
+    )
+    search_fields = (
+        'title',
+        'code',
+        'slug',
+        'description',
+        'page__title',
+        'page__code',
+        'page__slug',
+        'page__hero__name',
+        'page__hero__slug',
+    )
+    list_filter = ('is_published', 'page__hero', 'page')
+    ordering = ('page__hero__name', 'page__order', 'order', 'title')
+    autocomplete_fields = ('page',)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('page', 'page__hero')
+
+    @admin.display(description='Page', ordering='page__title')
+    def page_link(self, obj):
+        url = reverse('admin:heroes_heropage_change', args=[obj.page_id])
+        return format_html('<a href="{}">{}</a>', url, obj.page.title)
+
+    @admin.display(description='Hero', ordering='page__hero__name')
+    def hero_link(self, obj):
+        url = reverse('admin:heroes_hero_change', args=[obj.page.hero_id])
+        return format_html('<a href="{}">{}</a>', url, obj.page.hero.name)
