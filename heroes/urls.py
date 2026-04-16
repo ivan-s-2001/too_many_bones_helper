e95df84a6c669cfe44f4fcb19cc0afae78ee6a04
Ivan Smirnov <ivan.s.2001@yandex.ru>
2026-04-16 15:36:19 +0300

    tttt

--- heroes/urls.py
+++ heroes/urls.py
@@ -5,5 +5,6 @@ from .views import hero_detail
 app_name = 'heroes'
 
 urlpatterns = [
-    path('<slug:slug>/', hero_detail, name='detail'),
+    path('<slug:hero_slug>/', hero_detail, name='detail'),
+    path('<slug:hero_slug>/<slug:page_slug>/', hero_detail, name='page_detail'),
 ]
--- heroes/views.py
+++ heroes/views.py
@@ -1,18 +1,29 @@
+from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 from .models import Hero
 
 
-def hero_detail(request, slug):
+def hero_detail(request, hero_slug, page_slug=None):
     hero = get_object_or_404(
         Hero.objects.filter(is_published=True),
-        slug=slug,
+        slug=hero_slug,
     )
+    pages = hero.pages.filter(is_published=True).order_by('order', 'title')
+
+    if page_slug:
+        current_page = get_object_or_404(pages, slug=page_slug)
+    else:
+        current_page = pages.first()
+        if current_page is None:
+            raise Http404('No published pages found for this hero.')
 
     return render(
         request,
         'heroes/hero_detail.html',
         {
             'hero': hero,
+            'current_page': current_page,
+            'pages': pages,
         },
     )
