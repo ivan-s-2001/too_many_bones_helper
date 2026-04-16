fb0c04b76c2b219cabcfe196a716796e31cd2d8d
Ivan Smirnov <ivan.s.2001@yandex.ru>
2026-04-16 14:27:37 +0300

    upgrate heroadmin

--- heroes/admin.py
+++ heroes/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from .models import Hero, HeroPage, PageSection
 
 
-admin.site.register(Hero)
+@admin.register(Hero)
+class HeroAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'accent', 'order', 'is_published')
+    search_fields = ('name', 'slug', 'tagline', 'description', 'accent')
+    list_filter = ('is_published',)
+    ordering = ('order', 'name')
+
+
 admin.site.register(HeroPage)
 admin.site.register(PageSection)
